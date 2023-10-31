838e8540132ebe845913e59aa1a922542b73d7a6
Kaldo <dantekaled100@gmail.com>
2023-10-30 21:31:01 -0600

    feat: Primer commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div>
+          <p></p>
+        </div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
